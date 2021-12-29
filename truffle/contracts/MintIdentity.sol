61ec0089968f32781db88053dae203612fb73375
Ramona <ramonajenny.n@gmail.com>
2021-12-28 21:06:02 -0500

    Contract Meta Start

--- truffle/contracts/MintIdentity.sol
+++ truffle/contracts/MintIdentity.sol
@@ -1,16 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MintIdentity {
+contract MintIdentity is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    struct  Identity  {
-        string identity;
+    struct Identity {
+        bytes32 identity;
         string firstname;
         string lastname;
+        string aka;
         string organization;
         string slogan;
-        string alias;
         string description;
         string url;
         string bio;
@@ -21,37 +26,50 @@ contract MintIdentity {
         uint validationscore;
     }
 
-    constructor() {
+    mapping(uint256=> Identity) id_to_identity;
 
-    }
-    /*function mint(uint16 year, uint8 month, uint8 day, uint8 color, string memory title) internal {
-        uint256 tokenId = id(year, month, day);
+    string private _currentBaseURI;
 
-        id_to_date[tokenId] = Metadata(year, month, day, color, title);
-        _safeMint(msg.sender, tokenId);
+    constructor() ERC721("MintIdentity", "MINTIDENTITY") {
+        setBaseURI("https://papermasters.io/mintidentity/");
+
+        //mint Ramona Andrew and kids
     }
-    */
-    function minIdentity(
+
+    function setBaseURI(string memory baseURI) public onlyOwner() {
+        _currentBaseURI = baseURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _currentBaseURI;
+    }
+
+    function createIdentity(
         string memory first,
         string memory last,
+        string memory aka,
         string memory org,
         string memory slogan,
-        string memory descr,
+        string memory description,
         string memory url,
-        string memory bio) internal {
-            uint256 tokenId = Identity('test',
-                first,
-                last,
-                org,
-                slogan,
-                descr,
-                url,
-                bio,
-                block.timestamp,
-                block.timestamp,
-                false,
-                0,
-                0 );
+        string memory bio) private view returns(bytes32) {
+        return keccak256(abi.encodePacked(first, last, aka, org, slogan, description, url, bio, block.timestamp));
     }
 
+    function mintIdentity (
+        string storage first,
+        string memory last,
+        string memory aka,
+        string memory org,
+        string memory slogan,
+        string memory description,
+        string memory url,
+        string memory bio) internal {
+            uint256 tokenId =  _tokenIds.current();
+            bytes32 identity = createIdentity(first, last, aka, org, slogan, description, url, bio);
+            id_to_identity[tokenId] = Identity(identity,first, last, aka, org, slogan, description,
+                url, bio, block.timestamp, block.timestamp, false, 0, 0);
+            _tokenIds.increment();
+
+    }
 }
