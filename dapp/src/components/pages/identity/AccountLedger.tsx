0b6ba18b8c092c6b250d66dc894ce0a9b6718724
avonn <avonnied@gmail.com>
2022-05-11 23:29:39 -0400

    placeholder

--- dapp/src/components/pages/identity/AccountLedger.tsx
+++ dapp/src/components/pages/identity/AccountLedger.tsx
@@ -11,7 +11,7 @@ import {
     Text,
     Stack,
     Spacer,
-    Tooltip
+    Tooltip, VStack
 } from "@chakra-ui/react";
 import * as React from "react";
 import {FC, useEffect, useMemo} from "react";
@@ -21,6 +21,13 @@ import {useGetQueryMainnetQuery} from "../../../features/reactQuery/RTKQuery";
 import {ParamsURLInterface} from "../../../features/accountDB/AccountDBSlice.types";
 import moment from "moment";
 import {ethers} from "ethers";
+import {
+    Tag,
+    TagLabel,
+    TagLeftIcon,
+    TagRightIcon,
+    TagCloseButton,
+} from '@chakra-ui/react'
 
 export const AccountLedger:FC<ParamsURLInterface>=({chainIdURL, paramsWalletURL})=> {
     const getLedger = useGetQueryMainnetQuery({
@@ -46,123 +53,90 @@ export const AccountLedger:FC<ParamsURLInterface>=({chainIdURL, paramsWalletURL}
                 <Stack
                     w={'100%'}
                 >
-                    {getLedger.isSuccess && getLedger.data.map((el: any) => {
+
+                    {getLedger.isSuccess && getLedger.data.map((el: any, index: number) => {
                         const timeStampFormatted = moment(el.timestamp*1000).format('MMM DD YYYY, hh:mm:ss a');
                         const timeStampShort = moment(el.timestamp*1000).format('MMM DD YYYY');
                         const valueFormatted =
                              parseFloat(ethers.utils.formatEther(el.value)) + parseFloat(ethers.utils.formatEther(el.gasPrice))
-                        console.log("valueFormatted",valueFormatted)
                         return (
                             <Box
                                 flex={'max-content'}
                                 w={'100%'}
                                 //border={'1px solid red'}
+                                py={"5px"}
+                                bgColor={index%2 == 0 ? "white" : "#eeeeee"}
                                 borderBottom={'1px solid'}
-                                borderColor={'pmpurple.4'}
+                                borderColor={'pmpurple.13'}
                                 px={2}
+
+                                fontSize={"10pt"}
                             >
                                 <Stack>
-                                    <HStack>
-                                            <HStack>
-                                                <Text
-                                                pr={'2px'}
-                                                color={'pmpurple.13'}
-                                                fontSize={'14px'}
-                                                whiteSpace={'nowrap'}
-                                                >
-                                                    Transaction Hash:
-                                                </Text>
-                                                <Text
-                                                    isTruncated={true}
-                                                    width={'300px'}
-                                                    color={'pmpurple.13'}
-                                                    fontSize={'14px'}
-                                                    whiteSpace={'nowrap'}
-                                                >
-                                                    {el.hash}
-                                                </Text>
-                                            </HStack>
-                                        <Spacer/>
+
+
+                                        <Text
+                                            textAlign={'left'}
+                                            isTruncated={true}
+                                            color={'pmpurple.13'}
+                                            whiteSpace={'nowrap'}
+                                        >
+                                            <Tag bgColor={"pmpurple.10"} color={"white"}  fontSize={"9pt"}>
+                                                Tx
+                                            </Tag>  {el.hash}
+                                        </Text>
+
+
                                         <Tooltip
                                             label={timeStampFormatted}
                                             aria-label='A tooltip'
                                             bg={'pmpurple.8'}
                                         >
                                         <Text
+                                            pl={"20px"}
                                             //noOfLines={1}
-                                            fontSize={'14px'}
                                             color={'pmpurple.13'}
                                             fontWeight="400"
                                             whiteSpace={'nowrap'}
                                         >
-                                            {timeStampShort}
+                                            <Tag fontSize={"9pt"}  bgColor={"pmpurple.13"} color={"white"}>{timeStampShort}</Tag> <Tag fontSize={"9pt"} bgColor={"pmpurple.10"} color={"white"}>Amount</Tag> {valueFormatted}
                                         </Text>
                                         </Tooltip>
                                         <Box
                                             //border={'1px solid orange'}
-                                            bg={'pmpurple.2'}
+
                                             borderRadius={'2px'}
+                                            pl={"20px"}
                                         >
-                                            <Text
-                                                isTruncated={true}
-                                                width={'100px'}
-                                                color={'pmpurple.13'}
-                                                fontSize={'14px'}
-                                                whiteSpace={'nowrap'}
-                                                title={valueFormatted.toString()}
-                                            >
-                                                {valueFormatted}
-                                            </Text>
+
                                         </Box>
-                                    </HStack>
-                                    <HStack>
-                                    <HStack>
-                                        <Text
-                                            pr={'2px'}
-                                            color={'pmpurple.13'}
-                                            fontSize={'14px'}
-                                            whiteSpace={'nowrap'}
-                                        >
-                                            From:
-                                        </Text>
+
+
+
                                         <Text
                                             isTruncated={true}
-                                            width={'300px'}
                                             color={'pmpurple.13'}
-                                            fontSize={'14px'}
                                             whiteSpace={'nowrap'}
+                                            fontWeight="400"
                                         >
-                                            {el.from}
+                                          <Tag  fontSize={"9pt"} bgColor={"pmpurple.10"} color={"white"}>From</Tag>  {el.from}
                                         </Text>
-                                    </HStack>
-                                    <Spacer/>
-                                    <HStack>
-                                        <Text
-                                            pr={'2px'}
-                                            color={'pmpurple.13'}
-                                            fontSize={'14px'}
-                                            whiteSpace={'nowrap'}
-                                        >
-                                            To:
-                                        </Text>
+
+
                                         <Text
                                             isTruncated={true}
-                                            width={'300px'}
                                             color={'pmpurple.13'}
-                                            fontSize={'14px'}
                                             whiteSpace={'nowrap'}
-                                        >
-                                            {el.to !== null ?
+                                            fontWeight="400"
+                                        > <Tag  fontSize={"9pt"} bgColor={"pmpurple.10"} color={"white"}>To</Tag> {el.to !== null ?
                                                 el.to
-                                                :  <Text
+                                                :  <Box
+                                                display={"inline"}
                                                     color={'pmpurple.27'}
-                                                >
-                                                Contract Creation
-                                                </Text>
+                                                >Contract Creation</Box>
                                             }
                                         </Text>
-                                    </HStack>
-                                    </HStack>
+
                                 </Stack>
                             </Box>
                         )
