7cbe9c84411e88e33052dc5f2c07b82014905810
ramona <ramonajenny.n@gmail.com>
2022-05-14 21:48:32 -0400

    change computers

--- dapp/src/components/pages/Report.tsx
+++ dapp/src/components/pages/Report.tsx
@@ -26,17 +26,25 @@ import {
     ModalBody,
     ModalCloseButton,
     Container, VStack, SimpleGrid, GridItem, Select,
-    useBreakpointValue,
+    useBreakpointValue, border,
 } from '@chakra-ui/react';
 
 export const Report:FC=()=> {
-    return(
-   <Box>
-       <Heading>
-           Coming soon...
-       </Heading>
-        </Box>
-    )
-};
+
+    const[count, setCount] = useState(0)
+
+    const increament=()=>{
+        setCount(count+1);
+    }
+
+    const decreament=()=> {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+        return (
+            <Box>Comming Soon...</Box>
+        )
+    };
 
 export default Report;
