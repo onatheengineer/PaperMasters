a9d247d3f905c39170b17d34d1bc491642fccc5a
ramona <ramonajenny.n@gmail.com>
2022-05-21 00:07:38 -0400

    looking at testing doing some unit tests on the contract

---
+++ dapp/src/features/JSON/chainId.tsx
@@ -0,0 +1,71 @@
+
+
+export const chainIdJSON = {
+        "1":{
+            "rpc":[
+                "https://api.mycryptoapi.com/eth",
+                "https://rpc.flashbots.net/",
+                "https://eth-mainnet.gateway.pokt.network/v1/5f3453978e354ab992c4da79",
+                "https://cloudflare-eth.com/",
+                "https://mainnet-nethermind.blockscout.com/",
+                "https://nodes.mewapi.io/rpc/eth",
+                "https://main-rpc.linkpool.io/",
+                "https://mainnet.eth.cloud.ava.do/",
+                "https://ethereumnodelight.app.runonflux.io",
+                "https://rpc.ankr.com/eth",
+                "https://eth-rpc.gateway.pokt.network",
+                "https://main-light.eth.linkpool.io",
+                "https://mainnet.infura.io/v3/c97ad56e08674161a95ba16c6f855b6a"
+            ]
+        },
+        "3":{
+            "rpc":[
+                "https://ropsten.infura.io/v3/${c97ad56e08674161a95ba16c6f855b6a}"
+            ]
+        },
+        "4":{
+            "rpc":[
+                "https://rinkeby.infura.io/v3/${c97ad56e08674161a95ba16c6f855b6a}"
+            ]
+        },
+        "5":{
+            "rpc":[
+                "https://goerli.infura.io/v3/${c97ad56e08674161a95ba16c6f855b6a}",
+                "https://rpc.goerli.mudit.blog/"
+            ]
+        },
+        "1666600000":
+        {
+            "rpc":[
+            "https://api.harmony.one",
+            "https://s1.api.harmony.one",
+            "https://s2.api.harmony.one",
+            "https://s3.api.harmony.one",
+            "https://harmony-0-rpc.gateway.pokt.network",
+            "https://a.api.s0.t.hmny.io",
+            "https://api.s0.t.hmny.io",
+            "https://rpc.heavenswail.one/",
+            "https://api.fuzz.fi/",
+            "https://rpc.hermesdefi.io/"
+        ]
+        },
+        "1666600001":{
+            "rpc":[
+                "https://s1.api.harmony.one"
+            ]
+        },
+        "1666600002":{
+            "rpc":[
+                "https://s2.api.harmony.one"
+            ]
+        },
+        "1666700000": {
+            "rpc": [
+                "https://api.s0.b.hmny.io",
+                "https://api.s1.b.hmny.io",
+                "https://api.s2.b.hmny.io",
+                "https://api.s3.b.hmny.io"
+            ]
+        }
+    }
+
